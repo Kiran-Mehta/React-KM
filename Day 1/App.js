8d1e0dbdde951bef4be9e43967caef1ec1901f55
Kiran-Mehta <88717610+Kiran-Mehta@users.noreply.github.com>
2023-02-21 17:40:51 +0530

    Add files via upload

---
+++ Day 1/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Child from "./Child";
+import ChildData from "./ChildData";
+import ChilComponent from "./ChildComponent";
+import DisplayTime from "./DisplayTime";
+import Counter from "./Counter";
+import ReactForm from './components/Form'
+import UserList from "./UserList";
+import CountEffect from "./CountEffect"
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  const handleCallback = (childname) => {
+    setName({ childname });
+    alert("");
+  };
+
+  const handleCount = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="App">
+      {/* <h1>This is Parent Component</h1>
+      <ul>
+        <li>
+          <Child firstNameColor="green" lastNameColor="yellow" />
+        </li>
+      </ul>
+      <ChildData parentCallback={handleCallback} />
+      {JSON.stringify(name)} */}
+
+      {/* <ChilComponent count={count} onClick={handleCount} /> */}
+
+      {/* <DisplayTime />
+
+      <Counter /> */}
+
+      {/* <ReactForm/> */}
+
+      <UserList/>
+
+      {/* <CountEffect/> */}
+    </div>
+  );
+}
